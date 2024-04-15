92d290955e6f98ae1696a5dc36520e0579943c47
dmt041104111003 <daomanhtung4102003@gmail.com>
2024-04-15 16:52:43 +0700

    fisrt

---
+++ src/view/ViDu.java
@@ -0,0 +1,12 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class ViDu {
+public static void main(String[] args) {
+	JFrame jf = new JFrame();
+	jf.setTitle("Ví dụ");
+	jf.setSize(600,400);
+	jf.setVisible(true);
+}
+}
