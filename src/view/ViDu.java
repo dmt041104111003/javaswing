a23aab9de21d454e1576935224c93ee6a8fd7ab0
dmt041104111003 <daomanhtung4102003@gmail.com>
2024-04-15 17:04:47 +0700

    push

--- src/view/ViDu.java
+++ src/view/ViDu.java
@@ -6,7 +6,7 @@ public class ViDu {
 public static void main(String[] args) {
 	JFrame jf = new JFrame();
 	jf.setTitle("Ví dụ");
-	jf.setSize(600,400);
+	jf.setSize(600,100);
 	jf.setVisible(true);
 }
 }
